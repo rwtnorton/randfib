9751fefa036ccaa4c62148241911d71ae66e5cf2
Richard W. Norton <rwtnorton@gmail.com>
2023-02-12 11:36:51 -0800

    switch to generating single fib value instead of whole sequence

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use clap::Parser;
 use num_bigint::BigUint;
 use num_traits::{Zero, One};
-use rand::seq::IteratorRandom;
+use rand::Rng;
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
@@ -13,21 +13,39 @@ struct Args {
 fn main() {
     let mut rng = rand::thread_rng();
     let args = Args::parse();
-    let vs = fibs(args.n);
-    // for v in &vs {
-    //     println!("{}", v);
-    // }
-    let selection = vs.iter().choose(&mut rng).unwrap();
+    // Skip over the first two fibonacci values, 0 and 1,
+    // to align better with typical Scrum story point values.
+    let i: u64 = rng.gen_range(2..(args.n+2));
+    let selection = nth_fib(i);
     println!("{}", selection);
 }
 
-fn fibs(n: u64) -> Vec<BigUint> {
-    let mut results = vec![BigUint::zero(), BigUint::one()];
+fn nth_fib(n: u64) -> BigUint {
+    let mut f0 = BigUint::zero();
+    let mut f1 = BigUint::one();
     for _i in 0..n {
-        let len = results.len();
-        let v = &results[len-2] + &results[len-1];
-        results.push(v);
+        let f2 = &f0 + &f1;
+        f0 = f1.clone();
+        f1 = f2;
     }
-    results.drain(..2);
-    results
+    f0
+}
+
+#[test]
+fn test_nth_fib() {
+    let test_cases : Vec<(u64, BigUint)>= vec![
+        0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,
+        233, 377, 610, 987, 1597,
+    ].iter().enumerate().map(|(i, n)| {
+        (i as u64, BigUint::from(n.clone() as u64))
+    })
+        .collect();
+    for (given, expect) in test_cases {
+        let got = nth_fib(given);
+        assert_eq!(
+            got, expect,
+            "given: {}, expect: {}, got: {}",
+            given, expect, got,
+        );
+    }
 }
